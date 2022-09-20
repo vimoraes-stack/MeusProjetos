9762ee1565c72de8a018988b81b7d74359d7e789
Bzo <um_velho_gamer@outlook.com>
2022-09-20 14:40:28 -0300

    #Incluindo decisoes encadedas 2

--- DecisaoEncadeada2.py
+++ DecisaoEncadeada2.py
@@ -1,3 +1,19 @@
 nome = input("Digite o nome: ")
-idade = input("Digite a idade: ")
+idade = int(input("Digite a idade: "))
 doenca_infectocontagiosa = input("Suspeita de doença infectocontagiosa ? ").upper()
+if idade >= 65:
+    print("Paciente COM PRIORIDADE")
+    if doenca_infectocontagiosa == "SIM":
+        print("Encaminhe" + nome + "para a sala AMARELA")
+    elif doenca_infectocontagiosa == "NAO":
+        print("Encaminhe" + nome + "para a sala BRANCA")
+    else:
+        print(" Responda a supeita de doença infectocontagiosa com SIM ou NAO")
+if idade < 65:
+    print("Paciente COM PRIORIDADE")
+    if doenca_infectocontagiosa == "SIM":
+        print("Encaminhe" + nome + "para a sala AMARELA")
+    elif doenca_infectocontagiosa == "NAO":
+        print("Encaminhe" + nome + "para a sala BRANCA")
+    else:
+        print(" Responda a supeita de doença infectocontagiosa com SIM ou NAO")
