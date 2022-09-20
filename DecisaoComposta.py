567b25ab4f35501f627b4fa9ae9670d9b003f5b3
Bzo <um_velho_gamer@outlook.com>
2022-09-20 11:53:05 -0300

    #final de decisões compostas

--- DecisaoComposta.py
+++ DecisaoComposta.py
@@ -1,8 +1,13 @@
-nome=input("Digite o nome: ")
-idade=int(input("Digite a idade: "))
-if idade>=65:
-    print("O paciente " + nome + " POSSUI atendimento prioritário!")
+nome = input("Digite o nome: ")
+idade = int(input("Digite a idade: "))
+doenca_infectocontagiosa = input(" Suspeita de doença infectocontagiosa? ").upper()
+if idade >= 65 and doenca_infectocontagiosa == "SIM":
+    print("O paciente " + nome + " será direcionado para sala AMARELA - COM PRIORIDADE")
+elif idade < 65 and doenca_infectocontagiosa == "SIM":
+    print("O paciente "+nome+" será direcionado para sala AMARELA - SEM PRIORIDADE")
+elif idade >= 65 and doenca_infectocontagiosa == "NAO":
+    print("O paciente " + nome + " será direcionado para sala BRANCA - COM PRIORIDADE")
+elif idade < 65 and doenca_infectocontagiosa == "NAO":
+    print("O paciente "+nome+" será direcionado para sala BRANCA - SEM PRIORIDADE")
 else:
-    print("O paciente " + nome + " NÃO possui atendimento prioritário!")
-
-    # texto para entender o controle de versoes
+    print("Responda a suspeita de doença infectocontagiosa com SIM ou NAO")
